2689455c2e297fa2b8c3e0002f1b2b89e45eb265
Jihoon Kim <kjh618@kaist.ac.kr>
2021-06-11 19:56:37 +0900

    Add sound effects

--- src/ball.gd
+++ src/ball.gd
@@ -10,8 +10,10 @@ func _physics_process(delta: float) -> void:
     var collision := move_and_collide(velocity * delta)
 
     if collision != null:
+        $BounceSound.play()
         velocity = velocity.bounce(collision.normal)
         velocity = (velocity + collision.collider_velocity).normalized() * speed
 
         if collision.collider.is_in_group("brick"):
+            $BrickBreakSound.play()
             collision.collider.queue_free()
